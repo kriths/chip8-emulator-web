5892aa57796f694cd0fd3397d787466e5f1c5d8f
Thomas Krisch <thomas.krisch+git@gmail.com>
2021-11-14 12:28:57 +0100

    chore: enable tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.ts',
+    './public/index.html',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
